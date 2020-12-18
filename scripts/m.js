ee651aedfb1c4234294fa3f619d376f8743fba28
Miles Ford <milesford99@gmail.com>
2020-12-17 20:54:15 -0500

    Add files via upload

---
+++ scripts/m.js
@@ -0,0 +1,6 @@
+//alert ("You've come to the right place...")
+
+
+var headName = document.getElementById("nameheader");
+var promptname = prompt("Please enter your full name");
+headName.innerText = promptname;
